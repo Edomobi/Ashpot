ec5b942be0e21bcabe9b91e23687907953c63834
Edomobi <programmerphython88@gmail.com>
2022-06-23 14:57:17 -0700

    intial commit

---
+++ index.js
@@ -0,0 +1,97 @@
+// let name='uba';
+// console.log(name)
+// alert(name)
+// let userName= prompt('enter a username')
+// alert(userName)
+
+// let meassage = 'mr/s/n uba joseph';
+// alert(message);
+
+//javascript operations
+// mathematical /arithemetic
+// relational/comparison
+// logical  operations
+// assignment operations
+// bitwise operators
+
+
+//mathematical operators
+// Addition
+// multiplication
+// subtration
+// division
+// modulus % (this is used to get remainders of a caculation)
+// square
+
+// let a = 30
+// let b = 45
+// let sum = a+b
+// alert ('the sum of a and b + sum')
+// alert ('the product of a and b is'+product)
+// find the area of a traingle having a base of 10 and height of 45
+// given a = 20, b =56 c =2 d=0.5
+// find the product of a,b and c multiplied by the sum of d,a,c and divided by 10
+// find the moduls 50 and 3
+
+
+// // NUMBER 1
+// let base = 10
+// let height = 45
+// let area =1/2
+// let multiplication = base*height
+// let sum =base*height /1/2
+// console.log(sum)
+
+// NUMBER 2
+//a = 20, b=56, c=2,d=0.5
+// let a = 20
+// let b = 56let c =2
+// let d =0.5
+// let divide =10
+// let product =a*b*c
+// let add =d+a+c
+// let multiplied = a*b*c*d+a+c
+// let total = a*b*c * d+a+c /10
+// console.log(total)
+
+// let num1 = 50
+// let num2 = 2
+// let find = num1/num2
+// console.log(find)
+
+//DAY 2//
+//   let age = prompt('input your age')
+//   evaluate = age > 10;
+//   alert(evaluate)
+
+
+let a = 2 
+let b = 5
+ a = a + b 
+alert (a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
